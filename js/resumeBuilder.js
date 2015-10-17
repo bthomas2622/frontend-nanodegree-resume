78d3747099845e35388dc99ef33edd1323636f6e
Ben Thomas <bthomas2622@gmail.com>
2015-10-16 21:57:16 -0400

    starting to add javascript to resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// var awesomeThoughts = "I am Ben and I am AWESOME!";
+// console.log(awesomeThoughts);
+// var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+// console.log(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Ben Thomas");
+var formattedRole = HTMLheaderRole.replace("%data%", "Network Design Engineer and Front End Web Meddler");
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var bio = {"name" : "Ben Thomas",
+ "role" : "Network Design Engineer and Front End Meddler",
+ "contact-info" : {"Email" : "bthomas2622@gmail.com", "Phone" : "678-463-1402"},
+ "picture-url" : "mugshot.jpg",
+ "welcome-message" : "Welcome to my interactive resume",
+ "skills" : ["Too Cool for School", "like way too cool"],
+};
